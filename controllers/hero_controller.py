12e63f7d94f574e9fdc8c155270e1678179540e1
weewilsonboy <wilson.euan@gmail.com>
2023-07-31 12:00:49 +0100

    added sale of owned items for half price

--- controllers/hero_controller.py
+++ controllers/hero_controller.py
@@ -4,12 +4,24 @@ from models.courier import Courier
 from models.hero import Hero
 from models.item import Item
 from models.courier import Courier
+from app import db
 
 hero_blueprint = Blueprint("hero", __name__)
 
 
 @hero_blueprint.route('/hero')
 def hero_page():
-    heroes = Hero.query.all()
+    heroes = Hero.query.order_by(Hero.name).all()
     couriers = Courier.query.all()
     return render_template('/hero/index.jinja', heroes = heroes, couriers = couriers)
+
+@hero_blueprint.route('/sale/<id>')
+def item_sale(id):
+    
+    courier_to_edit = Courier.query.get(id)
+    item_being_sold = Item.query.get(courier_to_edit.item.id)
+    hero_selling = Hero.query.get(courier_to_edit.hero.id)
+    hero_selling.gold += (item_being_sold.cost/2)
+    courier_to_edit.sold = True
+    db.session.commit()
+    return redirect('/hero')
--- models/courier.py
+++ models/courier.py
@@ -8,6 +8,7 @@ class Courier(db.Model):
     id = db.Column(UUID(as_uuid = True), primary_key = True, default=uuid.uuid4)
     hero_id = db.Column(UUID(as_uuid = True), db.ForeignKey('heroes.id'))
     item_id = db.Column(UUID(as_uuid = True), db.ForeignKey('items.id'))
+    sold = db.Column(db.Boolean, server_default = 'false')
 
     def __repr__(self):
         return f"{self.id} {self.hero_id} {self.item_id}"
