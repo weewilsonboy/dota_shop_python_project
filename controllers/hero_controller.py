840eb7dd1642a1633e6133c7986c5202120056f4
weewilsonboy <wilson.euan@gmail.com>
2023-07-31 11:34:10 +0100

    added images of what items a hero owns

--- controllers/hero_controller.py
+++ controllers/hero_controller.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 from models.courier import Courier
 from models.hero import Hero
 from models.item import Item
+from models.courier import Courier
 
 hero_blueprint = Blueprint("hero", __name__)
 
@@ -10,5 +11,5 @@ hero_blueprint = Blueprint("hero", __name__)
 @hero_blueprint.route('/hero')
 def hero_page():
     heroes = Hero.query.all()
-    
-    return render_template('/hero/index.jinja', heroes = heroes)
+    couriers = Courier.query.all()
+    return render_template('/hero/index.jinja', heroes = heroes, couriers = couriers)
